a441f6edf647b022074aa27c1e48f6bab81383c3
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-09 16:30:38 -0500

    classwork

--- pythonProject/Class3/Step_4.py
+++ pythonProject/Class3/Step_4.py
@@ -16,18 +16,18 @@
 # We will query the file Step_4.csv, a file of population data per year per country:
 import csv
 
-with open("Step_4.csv") as population_csv:
-    csv_reader = csv.reader(population_csv, delimiter=',')
-
-    line_count = 0
-
-    for row in csv_reader:
-        if line_count == 0:
-            print("Column names are: " + str(row))
-            line_count += 1
-        line_count += 1
-
-print("Processed " + str(line_count) + " lines.")
+# with open("Step_4.csv") as population_csv:
+#     csv_reader = csv.reader(population_csv, delimiter=',')
+#
+#     line_count = 0
+#
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print("Column names are: " + str(row))
+#             line_count += 1
+#         line_count += 1
+#
+# print("Processed " + str(line_count) + " lines.")
 
 # We can do some basic tasks, for example, let's sum the "Population" column
 
@@ -38,9 +38,9 @@ with open("Step_4.csv") as population_csv:
         total += float(row[3]) #3 = 4th column, in this case, population, remember Python uses zero indexing
     print(format(total, 'f')) # format prints as float
     print(total) # without we print as engineering notation
-    # Now look at the data, 3 hundred billion? Ah, we have multiple years..
-
-# Task - Using Step_4.csv, complete the following, 1) Make a list of the years and the countries in the dataset,
-# how many years and countries are there? 2) Calculate the global population for every year in the dataset.
-# 3) Split the global dataset into a file for each country, with the filename the name of the country, in a new
-# directory using only Python
+#     # Now look at the data, 3 hundred billion? Ah, we have multiple years..
+#
+# # Task - Using Step_4.csv, complete the following, 1) Make a list of the years and the countries in the dataset,
+# # how many years and countries are there? 2) Calculate the global population for every year in the dataset.
+# # 3) Split the global dataset into a file for each country, with the filename the name of the country, in a new
+# # directory using only Python
