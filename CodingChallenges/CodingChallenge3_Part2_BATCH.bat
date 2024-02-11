4a2f6a51a1362e9c55aec9d01f377a73d228d495
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-10 20:33:08 -0500

    Update Part 2 files

--- CodingChallenges/CodingChallenge3_Part2_BATCH.bat
+++ CodingChallenges/CodingChallenge3_Part2_BATCH.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" CodingChallenge3_Part2.py arg1 arg2 arg3
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" NRS528_CodingChallenge3_Part2.py 1 2 3
 pause
--- CodingChallenges/NRS528_CodingChallenge3_Part2.py
+++ CodingChallenges/NRS528_CodingChallenge3_Part2.py
@@ -11,3 +11,16 @@
 ####################################
 
 import sys
+sys.stderr.write("Think carefully: what three numbers have the same sum and product?\n")
+user_guess = set()
+num_list = [int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])]
+actual = set()
+actual.update(num_list)
+for i in range(3):
+	number = int(input("Enter one number and press enter: "))
+	user_guess.add(number)
+ordered_guess = sorted(user_guess)
+if actual == set(ordered_guess):
+	print("Nice one.")
+else:
+	print("No... that's not right. The correct numbers were " + sys.argv[1] + ", " + sys.argv[2] + ", and " + sys.argv[3])
