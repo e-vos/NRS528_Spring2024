bbcabb7191be11d41c464544c9e43be831060ab9
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-11 21:03:04 -0500

    Completed Task 1 of CC3 Part 3

--- CodingChallenges/Coding_Challenge_3/NRS528_CodingChallenge3_Part3.py
+++ CodingChallenges/Coding_Challenge_3/NRS528_CodingChallenge3_Part3.py
@@ -11,31 +11,46 @@
 
 import csv
 
-# Calculate annual averages
+years = []		# Create a list to store YYYY from the date column
+annuals_dict = {}	# Dictionary for use later on...
 
-with open("co2-ppm-daily.csv") as co2:
+# Find unique YYYY values, to iterate through the ppm values
 
-	years = []					# Blank lists for years and ppm values
-	ppm = []
+with open(r"C:\Users\Elliot\Documents\Github\NRS528\NRS528_Spring2024\CodingChallenges\Coding_Challenge_3\co2-ppm-daily.csv") as co2:
 
-	next(co2)					# Skip the first row (headings)
+	next(co2)
 
-	for row in csv.reader(co2):			# Iterate through each row in the file
+	for row in csv.reader(co2):
 
-		date = row[0]				# Since we're working per row, row[0] will
-		value = float(row[1])			# always be a date and row[1] will always be
-		year = date.split('-')[0]		# a ppm value. Find year by splitting date into
-							# a list and grabing the first item in said list.
+		date = row[0]			# This block splits row[0] by hyphens into a list, then grabs the first 
+		year = date.split('-')[0]	# item in the list (i.e. the year in YYYY format).
+		years.append(year)		# Then add it to the years list
 
-		years.append(year)			# Each row, add the year to years[]
-		ppm.append(value)			# Each row, add the ppm value to averages[]. Even
-							# though it's still just a ppm value, later I'll
-							# average them.
+years_set = sorted(list(set(years)))		# Turn it into a set to get unique years, turn it back into a list, then
+						# sort it in ascending order.
 
-	years_set = list(set(years))			# Back to sets. We need it to be a set to have only
-	srt_years = sorted(years_set)			# unique year values. Then sort it (1958, 1959, etc.)
+# Now for iterating through the ppm column by the items in years_set
 
-averagesDict = {}					# Create a blank dictionary where I'll store unique years
-							# (from srt_years) and the respective ppm annual averages
+for year in years_set:
 
-for year in srt_years:
+	with open(r"C:\Users\Elliot\Documents\Github\NRS528\NRS528_Spring2024\CodingChallenges\Coding_Challenge_3\co2-ppm-daily.csv") as co2:
+
+		summed_values = float(0)	# Need float here because the ppm values are floats
+		count_values = 0		# Count can be an integer
+
+		next(co2)
+
+		for row in csv.reader(co2):
+
+			if str(year) in str(row[0]):			# Check if these are the values we're looking for 
+
+				summed_values += float(row[1])		# Sum all of the ppm values found with the current year index
+				count_values += 1			# Count how many ppm values have been summed
+
+			if count_values != 0:				# Best practice for the future, yes?
+
+				annual_avg = float(summed_values/count_values)		# Calculate average ppm value for the year
+				annuals_dict[year] = annual_avg				# Associate the average with the year, in the
+											# dictionary (e.g. 1959 | 300).
+
+# print(annuals_dict)				
