88514450291959ef78168be53edd3aaa59ec0f6b
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-12 10:14:11 -0500

    Complete Part 3

--- CodingChallenges/Coding_Challenge_3/NRS528_CodingChallenge3_Part3.py
+++ CodingChallenges/Coding_Challenge_3/NRS528_CodingChallenge3_Part3.py
@@ -84,8 +84,56 @@ with open(r"C:\Users\Elliot\Documents\Github\NRS528\NRS528_Spring2024\CodingChal
 
 # Seasonal average in Spring, Summer, Fall, and Winter
 
-spring_months = ["03", "04", "05"]	# Use the MM format for months
-summer_months = ["06", "07", "08"]
-fall_months = ["09", "10", "11"]
-winter_months = ["12", "01", "02"]
+ppm_values = []				# Reset the list from Task 2
 
+seasons = {'spring': ["03", "04", "05"], 'summer': ["06", "07", "08"],			# Create a seasons dictionary
+		'fall': ["09", "10", "11"], 'winter': ["12", "01", "02"]}
+
+monthly_dict = {}	# Storage for monthly averages
+seasonal_avgs = {}	# Storage for seasonal averages
+
+for month in all_months:
+
+	with open(r"C:\Users\Elliot\Documents\Github\NRS528\NRS528_Spring2024\CodingChallenges\Coding_Challenge_3\co2-ppm-daily.csv") as co2:
+
+		summed_values = float(0)
+		count_values = 0
+
+		next(co2)
+
+		for row in csv.reader(co2):
+
+			if str(month) in str(row[0]):			# Iterate by month
+
+				ppm = row[1]				# Same as Task 2
+				ppm_values.append(ppm)
+				summed_values += float(ppm)
+				count_values += 1
+
+		monthly_avg = float(summed_values/count_values)		# " "
+		monthly_dict[month] = monthly_avg
+
+# print(monthly_dict)
+
+for season, mm in seasons.items():					# Using the items() method here to get seasons dictionary
+									# items.
+	season_values = [monthly_dict[month] for mm in months]		# Extract values based on the month key in monthly_dict
+	seasonal_avgs[season] = sum(season_values)/len(season_values)	# Calculate seasonal averages and associate with season keys
+
+# print(seasonal_avgs)
+
+# Calculate the anomaly for each value in the dataset relative to the mean for the entire time series.
+
+anomalies = {}	# Dictionary to store anomaly values
+
+with open(r"C:\Users\Elliot\Documents\Github\NRS528\NRS528_Spring2024\CodingChallenges\Coding_Challenge_3\co2-ppm-daily.csv") as co2:
+
+	next(co2)
+
+	for row in csv.reader(co2):
+
+		position = row[1].index			# Get position of the current row. I could have used the date, but positions
+							# are unique. The date might not be.
+
+		anomaly = float(row[1]) - avg_ppm	# Calculate anomaly value for the current ppm value based on avg_ppm (Task 2)
+		anomalies[position] = anomaly		# Store the position as key and anomaly as its value in the dictionary
