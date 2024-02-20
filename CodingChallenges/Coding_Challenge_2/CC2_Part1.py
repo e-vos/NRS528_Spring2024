8ce5d1f67c87db86253f84cde81851ae5420961b
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-20 09:28:07 -0500

    Update CC2_Part1.py

--- CodingChallenges/Coding_Challenge_2/CC2_Part1.py
+++ CodingChallenges/Coding_Challenge_2/CC2_Part1.py
@@ -11,7 +11,16 @@
 
 init_list = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]			# Define the initial list
 
-selective_list = list(filter(lambda x: x < 5, init_list))	# Store values from init_list < 5 into selective_list;
-								# This is an alternative I came up with to a "for" loop.
+selective_list = []
+
+for i in init_list:
+
+    if i < 5:
+
+        selective_list.append(i)
+
+selective_list_oneline = list(filter(lambda x: x < 5, init_list))	# Store values from init_list < 5 into selective_list;
+                                                            # This is an alternative I came up with to a "for" loop.
 								
-print(selective_list)
+print("Results of for loop method: " + selective_list)
+print("Results of one-line method: " + selective_list_oneline)
