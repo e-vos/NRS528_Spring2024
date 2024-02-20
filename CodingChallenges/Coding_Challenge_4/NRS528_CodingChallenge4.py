b2b2aa6defa1c4044e6682a1a5c920f8ab93f89e
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-20 09:38:27 -0500

    Update NRS528_CodingChallenge4.py

--- CodingChallenges/Coding_Challenge_4/NRS528_CodingChallenge4.py
+++ CodingChallenges/Coding_Challenge_4/NRS528_CodingChallenge4.py
@@ -4,11 +4,22 @@
 # Description:
 #
 #	This file contains my answers to Coding Challenge 4.
+#
+#   In the first part I create an example shapefile for use in this exercise. It's a point feature class
+#       that contains five objects corresponding to five islands in Narragansett Bay.
+#
+#   In the second part, I showcase a tool called Create Thiessen Polygons by applying it to the example
+#       dataset.
+#
+#   I also make a comment on tuples that occurred to me when I was working on this assignment.
+
 
 import os
 import arcpy
 
-# Setup for creating the shapefile
+#########################
+# I. Create example data
+#########################
 
 your_path = os.path.dirname(os.path.realpath(__file__))                                 # Find the current directory
 shp_name = "cc4_points.shp"                                                             # Name the new shapefile
@@ -20,7 +31,7 @@ new_shape_file = arcpy.CreateFeatureclass_management(your_path, shp_name, geom_t
                                                                                                                 # U.S. ft but Python didn't like the foot coordinates
                                                                                                                 # I was giving it in fc_points.
 
-# "Prudence, Patience, Hope, and Despair. And Little Hog Island over there." ~ Roger Williams
+# "Prudence, Patience, Hope, and Despair. And Little Hog Island over there." ~ Colonial nursery rhyme
 # Define the coordinates for each island referenced in the rhyme
 
 fc_points = [
@@ -31,7 +42,7 @@ fc_points = [
     [-71.281009, 41.641571]
 ]
 
-for i in fc_points:	        # I didn't know if this was required or not. It seems like arcpy
+for i in fc_points:	            # I didn't know if this was required or not. It seems like arcpy
     for j in range(len(i)):     # automatically converts coordinate values to floats, as it did in
         i[j] = float(i[j])      # my initial testing, but I've included this loop anyway.
 
@@ -46,18 +57,20 @@ InsertCursor is a very useful Data Access class, allowing for the addition of ne
 tables. 
 
 SHAPE@XY is a tuple. You said at the beginning of the course that we would not use tuples,
-but by their nature they seem essential to vector data like in this example. Coordinates have
-a fixed number of values. In this case, just X and Y. InsertCursor allows for XYZ as well.
-
-By storing coordinates in tuples, their order and values are immutable. 
+but by their nature they seem essential to vector data like in this example.yB storing 
+coordinates in tuples, their order and values are immutable. 
 '''
+
+#############################################
+# II. Tool showcase: Create Thiessen Polygons
+#############################################
 
 outFeatureClass = "thiessen_polys.shp"
 out_shp = arcpy.CreateFeatureclass_management(your_path, outFeatureClass, "POLYGON", spatial_reference=4326)	# Again
 outFields = "ALL"
 
-arcpy.analysis.CreateThiessenPolygons(shp_name, out_shp, outFields)	# Use Create Thiessen Polygons on points to generate
-									# Voronoy cells corresponding to fc_points. From what
-									# I learned about them, this little example has no
-									# meaning. It just looks cool. I won't pretend to
-									# understand how they're calculated mathematically.
+arcpy.analysis.CreateThiessenPolygons(shp_name, out_shp, outFields)	    # Use Create Thiessen Polygons on points to generate
+                                                                        # Thiessen polygons corresponding to fc_points. From what
+                                                                        # I learned about them, this little example has no
+                                                                        # meaning. It just looks cool. I won't pretend to
+                                                                        # understand how they're calculated mathematically.
