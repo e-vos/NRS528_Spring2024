885ffb093cb39af5c42b8012f8d072d94b646cd2
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-23 13:08:41 -0500

    complete cc4

--- pythonProject/Class4/cc4.py
+++ pythonProject/Class4/cc4.py
@@ -1,10 +1,3 @@
-# Filename: NRS528_CodingChallenge4.py
-# Author: Elliot Vosburgh
-# Date: 18 February 2024
-# Description:
-#
-#	This file contains my answers to Coding Challenge 4.
-
 from arcpy import env
 from arcpy.sa import *
 import os
